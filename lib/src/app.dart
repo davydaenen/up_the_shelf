e6c185a945a0cd4094cc7cf4d2cd029d1e6e737f
Davy Daenen <davydaenen3@hotmail.com>
2021-10-18 15:00:28 +0200

    WIP: resizing for desktop/web

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -91,16 +91,23 @@ class MyApp extends StatelessWidget {
                   //     : ThemeMode.light,
                   themeMode: ThemeMode.system,
                   builder: (context, widget) => ResponsiveWrapper.builder(
-                    widget,
-                    maxWidth: double.infinity,
-                    minWidth: 480,
-                    defaultScale: true,
-                    breakpoints: [
-                      const ResponsiveBreakpoint.resize(480, name: MOBILE),
-                      const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-                      const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-                    ],
-                  ),
+                      widget,
+                      maxWidth: 750,
+                      minWidth: 450,
+                      defaultScale: true,
+                      breakpoints: [
+                        const ResponsiveBreakpoint.resize(450, name: MOBILE),
+                        const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+                        const ResponsiveBreakpoint.autoScale(1000,
+                            name: TABLET),
+                        const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+                        const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+                      ],
+                      background: Container(
+                          color: ThemeMode.system == ThemeMode.light
+                              ? AppTheme.lightBG
+                              : AppTheme.darkBG)),
+
                   home: Consumer<AuthProvider>(
                     builder: (_, authProviderRef, __) {
                       if (userSnapshot.connectionState ==
